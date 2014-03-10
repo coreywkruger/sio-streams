b3a64f3eb6d38fd5451a2eec624cc39ffd85a248
coreykruger <coreywkruger@gmail.com>
2014-03-09 22:31:26 -0700

    Init

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var io = require('socket.io').listen(server);
 
 io.configure(function () { 
 	io.set("transports", ["xhr-polling"]); 
-  	io.set("polling duration", 10); 
+  	io.set("polling duration", 100); 
 });
 
 var users = [];
