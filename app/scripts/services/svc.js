a02c7b7f1df8084aaef9b1b16443726e07134c5e
coreykruger <coreywkruger@gmail.com>
2014-03-10 18:23:57 -0700

    Init

--- app/scripts/services/svc.js
+++ app/scripts/services/svc.js
@@ -45,9 +45,7 @@ sioStreamsApp.provider("svc", function(){
 
 				self.mainSocket = io.connect('http://chat1-0.herokuapp.com');
 				//self.mainSocket = io.connect('192.168.0.2:5000');
-
-				self.mainSocket.emit('begin', {message: 'startup'});
-
+				console.log(self.mainSocket);
 				self.mainSocket.on('update', function (data) {
 					
 					self.outputs.outs.push( data );
